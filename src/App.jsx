3bc18bcd1901d5bfc20fdece45b41552f25c5a21
Buhaienko-Kate <buguzia89@gmail.com>
2021-12-10 20:26:45 +0200

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,12 @@ import third from './image/3rd.svg';
 const App = () => {
   const [authors, setAuthors] = useState([]);
 
-  const array = [first, second, third];
-
-  const info = authorsData
-    .slice()
-    .sort((a, b) => b.pageviews - a.pageviews)
-    .map((author, index, arrayImg) => {
-      if (index === arrayImg[index]) {
-        return { ...author, image: arrayImg[index] };
-      }
-      return author;
-    });
-  console.log(info);
+  const arrayImg = [first, second, third];
 
   const authorsInfo = authorsData
-    .slice()
-    .sort((a, b) => b.pageviews - a.pageviews)
-    .map((author, index) => {
-      if (index === 0) {
-        return { ...author, image: first };
-      }
-      if (index === 1) {
-        return { ...author, image: second };
-      }
-      if (index === 2) {
-        return { ...author, image: third };
-      }
-      return author;
-    });
+    .slice(0, 3)
+    .map((author, index) => ({ ...author, image: arrayImg[index] }))
+    .concat(authorsData.slice(3));
 
   const getCardsList = () => {
     setAuthors(authorsInfo);
